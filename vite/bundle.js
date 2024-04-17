dc734e94b013e69e3e48badea9768f6546767465
Matthias Dittgen <matthias.dittgen@transfermarkt.de>
2024-04-17 14:05:10 +0200

    Add bundles to show the problem

---
+++ vite/bundle.js
@@ -0,0 +1,24 @@
+var __defProp = Object.defineProperty;
+var __defNormalProp = (obj, key, value) =>
+  key in obj
+    ? __defProp(obj, key, {
+        enumerable: true,
+        configurable: true,
+        writable: true,
+        value,
+      })
+    : (obj[key] = value);
+var __publicField = (obj, key, value) => {
+  __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
+  return value;
+};
+(function () {
+  "use strict";
+  (function () {
+    var _a;
+    class MyClass {}
+    _a = Math.random();
+    __publicField(MyClass, _a, "random");
+    console.log(MyClass[Math.random()]);
+  })();
+})();
